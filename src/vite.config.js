1a7abe6dbe75c6c338facb6e1b097f251eed955e
harshpatel <harsh.patel991@gmail.com>
2022-09-11 16:52:31 -0500

    Define vite base path

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: "./",
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
