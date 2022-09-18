a1fec16b7c06f60b2b4f6b7e742345807c4ff2b3
MarbleMetrics <marblemetrics@gmail.com>
2022-09-18 01:27:36 -0500

    Add login with Google

---
+++ src/app/Http/Controllers/Auth/GoogleAuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+
+class GoogleAuthController extends Controller
+{
+
+    public function redirect()
+    {
+        return response('', 409)
+            ->header('X-Inertia-Location', Socialite::driver('google')->redirect()->getTargetUrl());
+    }
+
+    public function callback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = \App\Models\User::updateOrCreate([
+            'google_id' => $googleUser->id,
+        ], [
+            'name' => $googleUser->name,
+            'email' => $googleUser->email,
+            'email_verified_at' => Carbon::now(),
+            'google_id' => $googleUser->id,
+            'password' => encrypt(substr(md5(rand()), 0, 10))
+        ]);
+
+        Auth::login($user);
+        return redirect('/dashboard');
+    }
+}
