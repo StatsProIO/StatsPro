bc76988f2774eb34b4bc0f97d2cc0ed2312f1ecb
harshpatel <harsh.patel991@gmail.com>
2022-09-18 17:11:16 -0500

    Remove unused fonts and bundle requests together

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -18,18 +18,9 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400;700&display=swap" rel="stylesheet">
-
-
-
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600&display=swap" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
