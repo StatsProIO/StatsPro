9d9147ba24c06ff76fa27333f6955e3246fb792c
harshpatel <harsh.patel991@gmail.com>
2022-09-18 19:31:04 -0500

    Copy changes on home page

--- src/resources/js/Pages/Welcome.jsx
+++ src/resources/js/Pages/Welcome.jsx
@@ -41,7 +41,7 @@ export default function Welcome(props) {
                 <Grid container spacing={0} className="hero-row" sx={{ pt: 5, px: 1 }} justifyContent="center" >
                     <Grid item xl={5} lg={7} md={7} sm={11} xs={12} >
                         <Typography variant="h2" component="div" sx={{ fontWeight: 600, textAlign: 'center' }} gutterBottom>
-                            GDPR-compliant Google Analytics alternative without compromises
+                            GDPR-compliant analytics without compromises
                         </Typography>
                         <Typography color="#aaa"  variant="h6" component="div" align='center' sx={{pb: 10}}>
                             Get powerful analytics on your website <b>without</b> a cookie banner. Fully compliant with GDPR, CCPA and PECR. Hosted fully in the EU.
