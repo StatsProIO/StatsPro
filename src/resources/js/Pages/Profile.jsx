befc70a5c561cfde579fc8471298ae3b32c45767
harshpatel <harsh.patel991@gmail.com>
2022-09-20 20:59:53 -0500

    Add profile page

---
+++ src/resources/js/Pages/Profile.jsx
@@ -0,0 +1,62 @@
+import { Button, Grid, Typography, Box } from '@mui/material';
+import { Inertia } from '@inertiajs/inertia';
+import Authenticated from '@/Layouts/Authenticated';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Avatar from "@mui/material/Avatar";
+import * as React from "react";
+import {Head} from "@inertiajs/inertia-react";
+import {subscriptionPlans} from "@/helpers/subscriptions";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+export default function Profile(props) {
+    console.log(props);
+    return (
+        <Authenticated
+            auth={props.auth}
+            errors={props.errors}
+        >
+
+            <Head title="Profile" />
+
+            <Grid item lg={10}>
+                <Typography variant="h4"><b>Profile</b></Typography>
+                <Typography variant="h6" color="text.secondary">Everything about you!</Typography>
+            </Grid>
+
+
+            <Grid container justifyContent="center">
+                <Grid item md={5} sx={{mt: 2}}>
+
+                    <Box style={{ backgroundColor: '#fff' }} sx={{ p: 4 }} >
+                        <Box style={{ justifyContent: "center", display: "flex" }}>
+                            <Avatar alt={props.auth.user.email} sx={{ width: 128, height: 128 }}  />
+                        </Box>
+
+                        <Typography variant={"h4"} sx={{ pt: 2, display: {xs: 'none', sm: 'block'}}}>{props.auth.user.email}</Typography>
+                        <hr/>
+
+                        <Typography variant={"h6"} sx={{py: 1}}>
+                            <b>Current Subscription</b> { (subscriptionPlans.find(plan => plan.productId === props.subscription_status.subscription_product_id)).name}
+                        </Typography>
+
+                        <Typography variant={"h6"} sx={{py: 1}}>
+                            <b>Logged in with</b>
+                            {props.auth.user.google_id ?  (' Google') : (' Email & Password')}
+                        </Typography>
+
+                        <Typography variant={"h6"} sx={{py: 1}}>
+                            <b>Account Created</b> { new Date(props.auth.user.created_at).toLocaleString()}
+                        </Typography>
+
+                        <Button variant="contained" onClick={() => Inertia.get('subscriptions')} endIcon={<ArrowForwardIosIcon/>}><b>Manage Your Subscription</b></Button>
+
+                    </Box>
+                </Grid>
+            </Grid>
+
+
+
+
+        </Authenticated>
+    );
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -32,7 +32,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     //TODO: make sure the user has atleast 1 domain, otherwise redirect to manage-sites
     $domain = Domain::where('user_id', Auth::user()->id)->oldest()->first();
-    
+
 
     if($domain == null) {
         return Redirect::route('manage-domains');
@@ -42,7 +42,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
-//TODO: return only the domains that 
+//TODO: return only the domains that
 Route::get('/manage-domains', function () {
     return Inertia::render('ManageDomains', ['domains' => Domain::where('user_id', Auth::user()->id)->get()]);
 })->middleware(['auth', 'verified'])->name('manage-domains');
@@ -50,6 +50,10 @@ Route::get('/manage-domains', function () {
 Route::get('/add-domain', function () {
     return Inertia::render('AddDomain');
 })->middleware(['auth', 'verified'])->name('add-domain');
+
+Route::get('/profile', function () {
+    return Inertia::render('Profile', ['subscription_status' => App\Services\SubscriptionService::getSubscriptionStatus()]);
+})->middleware(['auth', 'verified'])->name('profile');
 
 //TODO: make sure that the domain is owned by this user
 Route::get('/domain/{domain_name}/script', function ($domainName) {
