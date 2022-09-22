568dc3d7590cf998b9a57939e2110df3d3df5141
harshpatel <harsh.patel991@gmail.com>
2022-09-22 00:37:00 -0500

    Adding About page

--- src/resources/js/Pages/Docs.jsx
+++ src/resources/js/Pages/Docs.jsx
@@ -8,6 +8,8 @@ export default function Docs() {
         <DocsLayout title={'Docs'}>
             <Typography variant={'h4'} sx={{py: 1}}>Welcome to the Marble Metrics docs!</Typography>
             <Typography sx={{py: 1}}>Here you'll find useful information on why and how to use Marble Metrics.</Typography>
+            <Typography sx={{py: 1}}><a href="/contact">Let us know</a> if there's some documentation you'd like to see.</Typography>
+
         </DocsLayout>
     );
 }
