17e70488dd79e47dd49044b0eaeef62b4495059d
MarbleMetrics <marblemetrics@gmail.com>
2022-09-21 21:45:42 -0500

    Add sidebar for docs pages

---
+++ src/resources/js/Layouts/DocsLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Guest from '@/Layouts/Guest';
+import {Grid, Typography} from '@mui/material';
+import ConvincingBanner from "@/Components/ConvincingBanner";
+import DocsList from "@/Components/DocsList";
+
+export default function DocsLayout({auth, title, children}) {
+    return (
+        <Guest auth={auth}>
+            <Grid container spacing={2} justifyContent={"center"} sx={{ py: 5, px: 1 }}>
+                <Grid item lg={2} md={3} sm={4} xs={12} order={{xs: 2, sm: 1}}>
+                    <DocsList/>
+                </Grid>
+                <Grid item lg={6} md={9} sm={8} xs={12} order={{xs: 1, sm: 2}}>
+                    <Typography variant="h3"><b>{title}</b></Typography>
+                    {children}
+                </Grid>
+            </Grid>
+            <ConvincingBanner/>
+        </Guest>
+    );
+}
--- src/resources/js/Pages/Docs.jsx
+++ src/resources/js/Pages/Docs.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import {Link, Typography} from '@mui/material';
-import General from "@/Layouts/General";
+import {Typography} from '@mui/material';
+import DocsLayout from "@/Layouts/DocsLayout";
 
 
 export default function Docs() {
     return (
-        <>
-
-            <Link href="/docs/about" underline="hover" color="inherit" align={"center"}>About</Link>
-            <Link href="/docs/adding-a-domain" underline="hover" color="inherit" align={"center"}>Adding a Domain</Link>
-            <Link href="/docs/faq" underline="hover" color="inherit" align={"center"}>FAQ</Link>
-            <Link href="/docs/getting-started" underline="hover" color="inherit" align={"center"}>Getting Started</Link>
-            <Link href="/docs/installing" underline="hover" color="inherit" align={"center"}>Installing</Link>
-
-        </>
+        <DocsLayout title={'Docs'}>
+            <Typography variant={'h4'} sx={{py: 1}}>Welcome to the Marble Metrics docs!</Typography>
+            <Typography sx={{py: 1}}>Here you'll find useful information on why and how to use Marble Metrics.</Typography>
+        </DocsLayout>
     );
 }
