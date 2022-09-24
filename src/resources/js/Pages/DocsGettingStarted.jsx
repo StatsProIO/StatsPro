191ae857c5b4096d3b26f02a92079c42b9cbedea
harshpatel <harsh.patel991@gmail.com>
2022-09-24 02:11:05 -0500

    Add Getting Started page

--- src/resources/js/Pages/DocsGettingStarted.jsx
+++ src/resources/js/Pages/DocsGettingStarted.jsx
@@ -7,9 +7,29 @@ import DocsLayout from "@/Layouts/DocsLayout";
 export default function DocsGettingStarted() {
     return (
         <DocsLayout title={'Getting Started'}>
-            <Typography sx={{py: 1}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec ex eget ex vestibulum dignissim non in nisi. Cras vulputate ligula eget faucibus lobortis. Proin molestie felis lorem, nec molestie nisi malesuada at. Sed a neque ut est eleifend blandit ac in odio. Ut eget justo suscipit, porttitor arcu quis, bibendum risus. Phasellus gravida cursus dolor id lobortis. Donec posuere odio tristique, pulvinar nibh non, volutpat massa.</Typography>
-            <Typography sx={{py: 1}}>Etiam nec pretium quam, quis dapibus dolor. Sed ut suscipit ante. Etiam nunc nunc, ullamcorper in elit et, lobortis faucibus tortor. Ut hendrerit vulputate nunc, mattis ultrices leo posuere sed. Vivamus vel pretium est. Nunc sodales et ex in tristique. Nam laoreet vitae augue quis tristique. Vivamus varius vitae turpis a mattis. Aenean ut interdum urna.</Typography>
-            https://umami.is/docs/about
+            <Typography variant={'h4'} sx={{py: 1}}>1. Create an account</Typography>
+            <Typography sx={{py: 1}}>Visit the <a href={'/register'}>Register</a> page to create an account with email and password or login with Google for a faster experience.</Typography>
+
+            <Typography variant={'h4'} sx={{py: 1}}>2. Add a domain</Typography>
+            <Typography sx={{py: 1}}>Once your registered, you'll be asked to add your first website domain. Enter in the URL to the website that you'd like to add your tracking to.</Typography>
+            <Typography sx={{py: 1}}>For example, if you'd like to track pageviews on test.com, enter in test.com in the box and click "Add Domain".</Typography>
+            <img width='70%' src='/images/getting-started-step-2.webp'/>
+
+            <Typography variant={'h4'} sx={{py: 1}}>3. Add the tracking script to your website</Typography>
+            <Typography sx={{py: 1}}>This next step will depend on your hosting setup. Take the unique code that's shown to you and copy it into your website portal. Depending on your setup, this could be Wordpress, Shopify, Blogger, Jekyll, or something similar. Basically, the idea is that you'll need ths script to load when someone visits your site.</Typography>
+            <img width='70%' src='/images/getting-started-step-3.webp'/>
+
+            <Typography variant={'h4'} sx={{py: 1}}>4. Visit your website</Typography>
+            <Typography sx={{py: 1}}>Click on "Start Collecting Data". Now, visit your website like a normal user would. This will trigger a pageview event to be sent to Marble Metrics and make it easy to verify everything is setup correctly.</Typography>
+
+            <Typography variant={'h4'} sx={{py: 1}}>5. Wait for a successful pageview</Typography>
+            <Typography sx={{py: 1}}>Now Marble Metrics will be waiting to see a successful pageview event from the script that you installed on your site. It should only take a few moments for the event to show up. If you are stuck on this screen, it could mean that something was incorrectly setup when installing the script. Double check to make sure you've installed it in the right place.</Typography>
+
+            <img width='60%' src='/images/getting-started-step-5.webp'></img>
+
+
+
+
         </DocsLayout>
     );
 }
