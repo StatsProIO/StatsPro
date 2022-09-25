cfa260d359ea5c26309436d843c3c1c51c6a6b59
MarbleMetrics <marblemetrics@gmail.com>
2022-09-25 02:10:00 -0500

    Don't allow duplicate domains

--- src/app/Http/Controllers/DomainsController.php
+++ src/app/Http/Controllers/DomainsController.php
@@ -10,24 +10,27 @@ class DomainsController extends Controller
 {
 
     public function postDomain(Request $request) {
-        //TODO: don't allow duplicate domains
-
         $domainName = $request->domain_name;
         $domainName = strpos($domainName, 'http') !== 0 ? "https://$domainName" : $domainName;
 
         if (filter_var($domainName, FILTER_VALIDATE_URL ) === FALSE) {
-            return response()->json(['message' => 'Not a valid URL.'], 400);
+            return redirect()->intended('/add-domain')->with('message', 'Not a valid URL.');
         }
 
         $domainNameHost = parse_url($domainName, PHP_URL_HOST);
 
         if($domainNameHost === null || $domainNameHost === '') {
-            return response()->json(['message' => 'Invalid URL. Missing host.'], 400);
+            return redirect()->intended('/add-domain')->with('message', 'Invalid URL. Missing host.');
         }
 
         //make sure there is at least 1 period in the domain
         if(preg_match('/.*\..*/', $domainNameHost) === 0) {
-            return response()->json(['message' => 'Invalid URL. Missing top-level domain (.com, .gov, etc).'], 400);
+            return redirect()->intended('/add-domain')->with('message', 'Invalid URL. Missing top-level domain (.com, .gov, etc).');
+        }
+
+        $existingDomains = Domain::where('domain_name', $domainNameHost)->count();
+        if ($existingDomains > 0) {
+            return redirect()->intended('/add-domain')->with('message', 'Domain ' . $domainNameHost . ' has already been registered.');
         }
 
         $domain = new Domain;
