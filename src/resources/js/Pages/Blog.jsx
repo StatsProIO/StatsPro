739a5ddf4375c6c76f4b2c1cd82c06d4a4c79392
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2022-11-12 17:25:04 -0600

    Add italy-illegal blog post

--- src/resources/js/Pages/Blog.jsx
+++ src/resources/js/Pages/Blog.jsx
@@ -15,6 +15,11 @@ const blogPosts = [
         title: 'Ethical Analytics',
         url: '/blog/ethical-analytics',
         cardImage: '/images/ethical-analytics.webp'
+    },
+    {
+        title: 'Italy Decalres Google Analytics Illegal',
+        url: '/blog/italy-google-analytics-illegal',
+        cardImage: '/images/illegal.webp'
     }
 ];
 
