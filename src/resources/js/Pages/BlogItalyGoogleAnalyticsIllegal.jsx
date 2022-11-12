64fa56925775f6ed5d5eca998fa25778e3be5394
MarbleMetrics <marblemetrics.com>
2022-11-12 17:25:04 -0600

    Add italy-illegal blog post

---
+++ src/resources/js/Pages/BlogItalyGoogleAnalyticsIllegal.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Typography} from '@mui/material';
+import BlogLayout from "@/Layouts/BlogLayout";
+
+
+export default function BlogItalyGoogleAnalyticsIllegal() {
+    return (
+        <BlogLayout title={'Italy Decalres Google Analytics Illegal'}>
+            <img src='/images/illegal.webp' width='100%' style={{paddingTop: '15px', paddingBottom: '15px'}}/>
+            <Typography sx={{py: 1}}>
+                The Italian Data Protection Agency (DPA) has fined Google €1,000,000 for violating the country's privacy laws. The DPA stated that Google failed to comply with its request for information about the company's processing of personal data.<br/><br/>
+                DPA investigation was prompted by a complaint from an Italian citizen who claimed that Google was processing his personal data without his consent. The DPA found that Google was processing the man's personal data without his consent and that the company had not provided any information about its data processing activities. DPA told Google that non-compliance is a "serious" violation of the country's data protection laws. The DPA also said it was concerned about the possibility of Google using its vast amounts of data to gain an unfair advantage in the
+                market.
+
+                The data protection authority has ordered Google to provide the man with information about his data processing activities and to stop processing his personal data without his consent. The DPA also fined Google €1,000,000 for violating the country's privacy laws.<br/><br/>
+                A careful review found Google Analytics to be in violation of GDPR law. The European Union's General Data Protection Regulation (GDPR) went into effect on May 25, 2018, and many businesses are still struggling to comply. One company that may not be compliant is Google Analytics. An investigation of Google Analytics has revealed that it violates the GDPR law on several counts.<br/><br/>
+
+                GDPR imposes significant fines on companies that break the law, including up to 4% of a company's global annual turnover or €20 million (each depending on which is the case). larger), whichever is larger. The GDPR also gives consumers the right to lodge a complaint with the supervisory authority if they believe that their rights have been violated.<br/><br/>
+
+                First, Google Analytics collects personally identifiable information without the consent of the tracked users. This is a clear violation of Article 7 of the GDPR, which requires users to be informed about and give their express consent to the collection of their personal data. Second, Google Analytics stores personal data indefinitely, even after the user has deleted their account. This violates article 17 of the RGPD, which requires that the data be deleted when the user requests it. Third, Google Analytics uses cookies to track users across the web, even if they don't use Google services. This violates Article 5 of the GDPR, which requires that cookies are only used for the purposes for which the user has given consent. Fourth, Google Analytics shares personal data with third-party advertisers, even if the user has not consented to this. This violates Article 6 of the GDPR, which requires data to be shared with third parties only if the user has given their explicit consent. Fifth, Google Analytics tracks the users who log into Google accounts, even if they have not consented to it. This violates Article 8 GDPR, according to which data from logged-in users is only collected if they have expressly consented.<br/><br/>
+                These are just a few examples of how Google Analytics violates the GDPR. If you use Google Analytics on your website, you must take steps to ensure you are GDPR compliant. The good news is that there are some simple steps you can take to ensure you meet the requirements. First, you must obtain explicit consent from all users who will be tracked by Google Analytics. This can be done by adding a GDPR-compliant consent form to your website. Secondly, you need to ensure that any personally identifiable information collected by Google Analytics is deleted at the user's request. This can be done by adding a GDPR-compliant takedown form to your website. Thirdly, you should stop using cookies to track users across the web. This can be done by disabling the cookie settings in Google Analytics. By following these steps, you can ensure you are GDPR compliant and avoid potential penalties. <br/><br/>
+
+                Alternatively, you can use an alternative to Google Analytics that is built from the ground up to be compliant will all worldwide data regulations. <br/><br/>
+
+                Marble Metrics is compliant with GDPR and all other data privacy laws by default. No need to take special precautions and you still get great analytics about visitors to your website. <br/><br/>
+
+                Ultimately, the world is trending towards a more data-conscious state and users are becoming aware of the number of rights they have given up. Regulation will continue to increase to put power back into the hands of users. Stay at the forefront with Marble Metrics.
+            </Typography>
+
+        </BlogLayout>
+
+    );
+}
