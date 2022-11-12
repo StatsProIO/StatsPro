f50a1eb0aa60c890e48ac19927b4a6ea5d6ed428
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2022-11-12 15:29:13 -0600

    Add blog posts

---
+++ src/resources/js/Pages/Blog.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Card, CardContent, CardMedia, Typography} from '@mui/material';
+import General from "@/Layouts/General";
+import {Box} from "@mui/system";
+import {Grid} from '@mui/material';
+
+
+const blogPosts = [
+    {
+        title: 'Deploying From Github Actions to Stackhero',
+        url: '/blog/deploying-from-github-actions-to-stackhero',
+        cardImage: '/images/deploying-from-github-actions-to-stackhero.avif'
+    },
+    {
+        title: 'Ethical Analytics',
+        url: '/blog/ethical-analytics',
+        cardImage: '/images/ethical-analytics.webp'
+    }
+];
+
+export default function Blog() {
+    return (
+        <General title={'Blog'}>
+            <Box sx={{py: 4}}>
+                <Grid container spacing={2}>
+
+                {blogPosts.map(blogPost => {
+                    return (
+                        <Grid item md={6} key={blogPost.url}>
+                            <a href={blogPost.url}>
+                            <Card >
+                                <CardMedia
+                                    component="img"
+                                    image={blogPost.cardImage}
+                                />
+                                <CardContent>
+                                    <Typography variant={'h5'} sx={{py: 1}}>{blogPost.title}</Typography>
+                                </CardContent>
+                            </Card>
+                            </a>
+                        </Grid>
+                    );
+                })}
+                </Grid>
+            </Box>
+        </General>
+    );
+}
