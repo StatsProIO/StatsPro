600d525105d7500646b49a138a2751becd95c51e
StatsProIO <statspro.io>
2022-11-12 18:04:47 -0600

    Add alternatives blog post

--- src/resources/js/Pages/Blog.jsx
+++ src/resources/js/Pages/Blog.jsx
@@ -20,7 +20,14 @@ const blogPosts = [
         title: 'Italy Decalres Google Analytics Illegal',
         url: '/blog/italy-google-analytics-illegal',
         cardImage: '/images/illegal.webp'
+    },
+    {
+        title: 'Top 5 Google Analytics Alternatives',
+        url: '/blog/top-5-google-analytics-alternatives',
+        cardImage: '/images/alternative.webp'
     }
+
+
 ];
 
 export default function Blog() {
