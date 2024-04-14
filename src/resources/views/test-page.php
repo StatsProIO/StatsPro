c8fe166ac530bdf136a6a9fef33220b7421293db
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2024-04-13 19:04:47 -0500

    Remove phpinfo from test-page

--- src/resources/views/test-page.php
+++ src/resources/views/test-page.php
@@ -1,3 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
 
-phpinfo();
+    <script data-domain="mydomain.com" src="/js/broadcaster.js" async></script>
+
+  </head>
+  <body>
+    <main>
+        <h1>Test Page</h1>
+        <a href="/test-page/nested">Go to nested page</a>
+
+        <button onclick="history.pushState({}, '', 'test-page/nested');">Go to nested page via pushState</button>
+
+        <button onclick="history.replaceState({}, '', 'test-page/nested');">Go to nested page via replaceState</button>
+
+    </main>
+  </body>
+</html>
