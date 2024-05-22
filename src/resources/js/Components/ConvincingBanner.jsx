91554df7f9f7d2e3d1025c287a883ae05bea1c23
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2024-05-21 22:20:54 -0500

    Update convincing banner

--- src/resources/js/Components/ConvincingBanner.jsx
+++ src/resources/js/Components/ConvincingBanner.jsx
@@ -11,11 +11,11 @@ export default function ConvincingBanner() {
             <Grid item xs={12} sx={{backgroundColor: '#334a71'}}>
                 <Grid container justifyContent={"center"} sx={{py: 3}}>
                     <Grid item lg={6} md={11} xs={12} sx={{color: '#fff', px: 1}}>
-                        <Typography variant={'h4'} fontWeight={'bold'}>The most EU-friendly analytics system.</Typography>
-                        <Typography variant={'h6'}>Start for free today, no credit card required.</Typography>
+                        <Typography variant={'h4'} fontWeight={'bold'}>The easiest analytics you've ever used.</Typography>
+                        <Typography variant={'h6'}>No cookie banner. No technical jargon.</Typography>
 
                         <Button onClick={() => Inertia.get('/register')} variant="contained" size="large" style={{fontSize: '1.1em'}} endIcon={<ArrowForwardIosIcon />}
-                                sx={{px: 2, py: 1, my: 2}} fontWeight="bold"><b>Get Started For Free</b></Button>
+                                sx={{px: 2, py: 1, my: 2}} fontWeight="bold"><b>Start today. No credit card required.</b></Button>
                     </Grid>
                 </Grid>
             </Grid>
