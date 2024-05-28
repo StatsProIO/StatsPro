519eb14e99cc158cd4f93198447dc8a0e5ba9811
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2024-05-28 01:27:35 -0500

    Add broadcaster back to app

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -74,6 +74,7 @@
         @vite('resources/js/app.jsx')
         @inertiaHead
 
+        <script data-domain="stats.io" src="{{ config('app.url') }}/js/broadcaster.js" async> </script>
 
     </head>
     <body class="font-sans antialiased" style="background-color: #f7f9fc">
