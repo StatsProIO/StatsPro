7c2367b848c414850597ada3c20a09b8b9fe2431
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2024-06-08 00:38:39 -0500

    Own domain is statspro.io

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -50,7 +50,7 @@
         @vite('resources/js/app.jsx')
         @inertiaHead
 
-        <script data-domain="stats.io" src="{{ config('app.url') }}/js/broadcaster.js" async> </script>
+        <script data-domain="statspro.io" src="{{ config('app.url') }}/js/broadcaster.js" async> </script>
 
     </head>
     <body class="font-sans antialiased" style="background-color: #f7f9fc">
