200e5e507b7d3a93a7b70252cddb3a788a44037b
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2024-06-12 00:01:22 -0500

    Redesign HorizontalBarChart

--- src/resources/js/Components/HorizontalBarChart.jsx
+++ src/resources/js/Components/HorizontalBarChart.jsx
@@ -1,6 +1,6 @@
 import {Box, Grid, Typography} from "@mui/material";
 import {scaleLinear} from "d3-scale";
-import {Fragment} from "react";
+import React, {Fragment} from "react";
 
 
 export default function HorizontalBarChart({ valueTitle, data, totalValue, colorMin, colorMax, labelKey }) {
@@ -22,25 +22,24 @@ export default function HorizontalBarChart({ valueTitle, data, totalValue, color
     return (
         <>
             <Grid container>
-                <Grid item xs={1} textAlign="center">
-                    <Typography variant="subtitle1" color="text.secondary">{valueTitle}</Typography>
+                <Grid item xs={12} textAlign="right">
+                    <Typography textAlign={'right'} variant="subtitle2" color="text.secondary">{valueTitle}</Typography>
                 </Grid>
             </Grid>
             <Grid container alignItems="center"
                 justifyContent="center">
 
                 {data.map((dataItem) =>
-
                     <Fragment key={dataItem.label + "-bar"}>
-                        <Grid item xs={1} justifyContent="center" >
-                            <Typography textAlign="center">{dataItem.count}</Typography>
+                        <Grid item xs={8} sx={{ pt: 2}}>
+                            <Typography variant={'body2'} style={{textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap', textAlign: 'left'  }} sx={{ py: 1 }}>{dataItem.label ?? dataItem[labelKey]}</Typography>
                         </Grid>
-                        <Grid item xs={11} >
-                            <Box sx={{ m: 1 }}>
-                                <Box style={{ backgroundColor: '#eee', borderTopRightRadius: '10px', borderBottomRightRadius: '10px' }}>
-                                    <Typography style={{ position: 'absolute', maxWidth: '15em', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'  }} sx={{ py: 1, pl: 1 }} display="inline">{dataItem.label ?? dataItem[labelKey]}</Typography>
-                                    <Box style={{ backgroundColor: colorScale(dataItem.count), width: (dataItem.count / totalValue) * 100 + '%', borderTopRightRadius: '10px', borderBottomRightRadius: '10px' }} sx={{ py: 1, pl: 1 }}>&nbsp;</Box>
-                                </Box>
+                        <Grid item xs={4} sx={{ pt: 2}}>
+                            <Typography variant={'body2'} textAlign="right">{dataItem.count}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Box style={{ backgroundColor: '#eee', borderTopRightRadius: '5px', borderBottomRightRadius: '5px' }}>
+                                <Box style={{ backgroundColor: colorScale(dataItem.count), width: (dataItem.count / totalValue) * 100 + '%', height: '5px', borderTopRightRadius: '5px', borderBottomRightRadius: '5px' }}>&nbsp;</Box>
                             </Box>
                         </Grid>
                     </Fragment>
