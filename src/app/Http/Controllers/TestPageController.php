d728dc2666354a3ee05cd6001cd8cb762383d26c
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2024-06-30 21:40:06 -0500

    enable test page

--- src/app/Http/Controllers/TestPageController.php
+++ src/app/Http/Controllers/TestPageController.php
@@ -8,7 +8,8 @@ class TestPageController extends Controller
 {
     public function get()
     {
-        return view('test-page');
+        return phpinfo();
+//        return view('test-page');
     }
 
      public function getNested()
