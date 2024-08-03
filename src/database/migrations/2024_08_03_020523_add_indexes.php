a9ed0d45cae18c269cad726cbbfda6cd69961929
StatsProIO <113157737+StatsProIO@users.noreply.github.com>
2024-08-02 21:15:35 -0500

    Add domain indexes

---
+++ src/database/migrations/2024_08_03_020523_add_indexes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            $table->index(['user_id']);
+            $table->index(['domain_name', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            $table->dropIndex(['user_id']);
+            $table->dropIndex(['domain_name', 'user_id']);
+        });
+    }
+};
